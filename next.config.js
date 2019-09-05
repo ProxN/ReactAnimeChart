acb3e68e1cddee2c6dd21560336e9526f9989d42
Proxn <gzproxn@gmail.com>
2019-09-06 00:55:58 +0100

    add exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-// next.config.js
 const withCSS = require('@zeit/next-css')
-
-module.exports = withCSS();
+module.exports = withCSS({
+  cssModules: true,
+  exportPathMap: function () {
+    return {
+        '/': { page: '/',query:{name:'SUMMER'} },
+        '/anime': { page: '/anime'},
+    }
+  }
+})
