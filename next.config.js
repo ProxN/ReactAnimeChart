7d8d0f4fc2b7f24605b6b11c83537d47bad3f075
Proxn <gzproxn@gmail.com>
2019-11-11 19:42:30 +0100

    current season

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withCSS = require("@zeit/next-css");
+const { Season } = require("./utils/getSeason");
+
 module.exports = withCSS({
   cssModules: true,
   exportPathMap: function() {
     return {
-      "/": { page: "/anime", query: { name: "FALL" } },
+      "/": { page: "/anime", query: { name: Season() } },
       "/WINTER": { page: "/anime", query: { name: "WINTER" } },
       "/SPRING": { page: "/anime", query: { name: "SPRING" } },
       "/SUMMER": { page: "/anime", query: { name: "SUMMER" } },
